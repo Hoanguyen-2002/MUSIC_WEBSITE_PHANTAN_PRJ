64f49ab519f39bb9965d0ef09121b4519416490c
Nguyễn Việt Hòa <84431330+Hoanguyen-2002@users.noreply.github.com>
2023-06-25 11:39:39 +0700

    Update src file

--- Music_Website_Enduser/src/components/Error.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Error = () => (
-  <div className="w-full flex justify-center items-center">
-    <h1 className="font-bold text-2xl text-white">Một số thứ đã bị lỗi. Vui lòng thử lại!</h1>
-  </div>
-);
-
-export default Error;
