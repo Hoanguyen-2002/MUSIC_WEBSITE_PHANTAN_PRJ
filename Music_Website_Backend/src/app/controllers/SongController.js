5e735fee2031e6720fd88bed03e01a2982868105
TrungKienz <kien.bt207710@sis.hust.edu.vn>
2023-06-28 11:10:36 +0700

    update CRUD songData

--- Music_Website_Backend/src/app/controllers/SongController.js
+++ Music_Website_Backend/src/app/controllers/SongController.js
@@ -71,6 +71,70 @@ class songController {
                 res.status(500).json({ error: 'Failed to add test case' });
             });
     }
+    delete(req, res, next) {
+        songModel
+            .deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    findByName(req, res) {
+        const name = req.params.songName;
+        songModel.find({ name }, (err, item) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send(err);
+            } else if (!item) {
+                res.status(404).send('Item not found');
+            } else {
+                res.send(item);
+            }
+        });
+    }
+
+    updateTest(req, res) {
+        const SongDataBody = req.body;
+        const updatedSongData = {
+            id: String(SongDataBody?.id),
+            thumb: String(SongDataBody?.thumb),
+            artist: String(SongDataBody?.artist),
+            artistIds: String(SongDataBody?.artistIds),
+            block: String(SongDataBody?.block),
+            hasVideo: Boolean(SongDataBody?.hasVideo),
+            videoLink: String(SongDataBody?.videoLink),
+            name: String(SongDataBody?.name),
+            genre: String(SongDataBody?.genre),
+            
+        };
+  
+        songModel
+        .findOneAndUpdate(
+            { patientID: SongDataBody.patientID },
+            updatedSongData,
+            { new: true },
+        )
+        .then((test) => {
+            if (test) {
+                console.log('Updated song in the database:', test);
+                res.status(200).json({
+                    message: 'Test case updated successfully',
+                    test,
+                });
+            } else {
+                console.log('Song not found');
+                res.status(404).json({ error: 'Song not found' });
+            }
+        })
+        .catch((err) => {
+            console.error(
+                'Error updating song in the database:',
+                err,
+            );
+            res.status(500).json({
+                error: 'Failed to update song',
+            });
+        });
+    }
 }
 
 module.exports = new songController();
