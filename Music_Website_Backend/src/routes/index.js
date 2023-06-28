7413aac641b750aef8c5e11abf980ba52942414e
TrungKienz <kien.bt207710@sis.hust.edu.vn>
2023-06-28 11:42:14 +0700

    update CORS and update main router for artist

--- Music_Website_Backend/src/routes/index.js
+++ Music_Website_Backend/src/routes/index.js
@@ -1,10 +1,12 @@
 const userRouter = require('./User');
 const songRouter = require('./Song');
+const artistRouter = require('./Artist');
 
 function routes(app) {
     app.use('/user', userRouter);
     app.use('/song', songRouter);
-    
+    app.use('/artist', artistRouter);
+
     app.get('/', (req, res) => {
         res.send('Hello World!');
     });
