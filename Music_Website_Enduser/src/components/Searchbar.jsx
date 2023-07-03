5bcda4a5b56a728c0f4714802b2db5718fc89a89
quangthanghust <thang.tq207701@sis.hust.edu.vn>
2023-07-03 16:15:13 +0700

    Update thông tin

--- Music_Website_Enduser/src/components/Searchbar.jsx
+++ Music_Website_Enduser/src/components/Searchbar.jsx
@@ -15,7 +15,7 @@ const Searchbar = () => {
   return (
     <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
       <label htmlFor="search-field" className="sr-only">
-        Search all files
+        Tìm kiếm tên bài hát, ca sĩ,...
       </label>
       <div className="flex flex-row justify-start items-center">
         <FiSearch aria-hidden="true" className="w-5 h-5 ml-4" />
--- Music_Website_Enduser/src/pages/Search.jsx
+++ Music_Website_Enduser/src/pages/Search.jsx
@@ -12,14 +12,14 @@ const Search = () => {
 
   const songs = data?.tracks?.hits?.map((song)=>song.track);
 
-  if (isFetching) return <Loader title={`Searching ${searchTerm}..`} />;
+  if (isFetching) return <Loader title={`Tìm kiếm ${searchTerm}..`} />;
 
   if (error) return <Error />;
 
   return (
     <div className="flex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
-        Showing results for <span className='font-black'>{searchTerm}</span>
+        Hiển thị kết quả cho <span className='font-black'>{searchTerm}</span>
       </h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
