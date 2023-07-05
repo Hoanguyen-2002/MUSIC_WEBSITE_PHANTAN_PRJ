17761beb6fccd93716891273e61043f8c70fd26c
Hoà Nguyễn <viethoah928@gmail.com>
2023-07-06 01:38:51 +0700

    update login

--- Music_Website_Enduser/src/pages/Login.jsx
+++ Music_Website_Enduser/src/pages/Login.jsx
@@ -1,12 +1,35 @@
 import React, { useState } from "react";
-
+import axios from 'axios';
 export const Login = (props) => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
 
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(email);
+  // };
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email);
+    //console.log("hello");
+    
+    // Tạo một đối tượng chứa dữ liệu đăng nhập
+    const loginData = {
+      email: email,
+      password: pass
+    };
+    
+    // Gửi yêu cầu đăng nhập đến API
+    axios.post("http://localhost:3000/user/login", loginData)
+      .then(response => {
+        // Xử lý phản hồi từ API
+        console.log(response.data);
+      })
+      .catch(error => {
+        // Xử lý lỗi nếu có
+        console.error(error);
+      });
   };
 
   return (
